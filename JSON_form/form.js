167ff0e26f279731671abec8ac2f5793d2f483e4
Easttown2000 <sindreostby@gmail.com>
2023-02-16 09:33:21 +0100

    made the file download the resulting json file

--- JSON_form/form.js
+++ JSON_form/form.js
@@ -5,8 +5,41 @@ formEvent.addEventListener("submit", event => {
     const formData = new FormData(formEvent);
     var object = {};
     formData.forEach((value, key) => object[key] = value);
-    object
-    var json = JSON.stringify(object)
     object["integrations"] = object["integrations"].split(",").map(s => s.trim())
-    console.log(object);
+    var filename = object["name"]+".json";
+    var json = JSON.stringify(object)
+    saveTextToFile(json,filename)
 });
+
+function saveTextToFile(text, name) {
+
+  // file setting
+  const type = "text/plain";
+
+  // create file
+  const a = document.createElement("a");
+  const file = new Blob([text], { type: type });
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
+
+function saveJsonObjToFile() {
+  const saveObj = { "a": 3 }; // tmp
+
+  // file setting
+  const text = JSON.stringify(saveObj);
+  const name = "sample.json";
+  const type = "text/plain";
+
+  // create file
+  const a = document.createElement("a");
+  const file = new Blob([text], { type: type });
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
