2b6678508e1643394aecedc62a1dee2e72b60839
OlaViken1 <ola.viken@ntnu.no>
2023-04-17 21:12:36 +0200

    Added the json file from the excel parser
    Added the json files and excel parser to the branch.

---
+++ Excelparser/Excelbehandler/ParsedItemApplicationList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excelbehandler
+{
+    internal class ParsedItemApplicationList
+    {
+        public string Name { get; set; } = string.Empty;
+        public string SystemNumber { get; set; } = string.Empty;
+        public string ServiceCaretaker { get; set; } = string.Empty;
+        public string Service { get; set; } = string.Empty;
+        public string ServiceOwner { get; set; } = string.Empty;
+        public string BusinessAreaLevelOne { get; set; } = string.Empty;
+        public string BusinessAreaLevelTwo { get; set; } = string.Empty;
+        public string Criticality { get; set; } = string.Empty;
+        public string UserChannel { get; set; } = string.Empty;
+        public string DatabaseType { get; set; } = string.Empty;
+        public string Environment { get; set; } =string.Empty;
+        public string AccessControll { get; set; } = string.Empty;
+        public string Datasource { get; set; } = string.Empty;
+    }
+}
---
+++ Excelparser/Excelbehandler/ParsedItemDataset.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excelbehandler
+{
+    internal class ParsedItemDataset
+    {
+        // A  very simple class to put the strings from the rows in excel
+        public string DataSetName { get; set; } = string.Empty;
+        public string DataSetOwner { get; set; } = string.Empty;
+        public string Municipality { get; set; } = string.Empty;
+        public string Business { get; set; } = string.Empty;
+        public string ContainsPII { get; set; } = string.Empty;
+        
+        public string HowIsItStored { get; set; } = string.Empty;
+        public string WhereIsIt { get; set; } = string.Empty;
+        public string DataProtocol { get; set; } = string.Empty;
+
+        public string HowOften { get; set; } = string.Empty;
+        public string HowPrecise { get; set; } = string.Empty;
+        public string OpennessDegree { get; set; } = string.Empty;
+        public string HasApi { get; set; } = string.Empty;
+        public string URLAPI { get; set; } = string.Empty;
+        public string Commments { get; set; } = string.Empty;
+        public string Datasource { get; set; } = string.Empty;
+
+    }
+}
