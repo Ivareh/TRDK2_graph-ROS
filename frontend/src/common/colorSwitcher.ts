64e2114b2c731a0b1c996dbe5c48f286eb87482f
Ivareh <69577035+Ivareh@users.noreply.github.com>
2023-04-24 14:34:29 +0200

    trd-33 changed iri in colorswitcher to match us

--- frontend/src/common/colorSwitcher.ts
+++ frontend/src/common/colorSwitcher.ts
@@ -1,38 +1,38 @@
 const colorSwitcher = (color: string) => {
   switch (color) {
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B1':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B1':
       return '#E5243B';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B2':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B2':
       return '#DDA63A';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B3':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B3':
       return '#4C9F38';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B4':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B4':
       return '#C5192D';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B5':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B5':
       return '#FF3A21';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B6':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B6':
       return '#26BDE2';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B7':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B7':
       return '#FCC30B';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B8':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B8':
       return '#A21942';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B9':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B9':
       return '#FD6925';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B10':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B10':
       return '#DD1367';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B11':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B11':
       return '#FD9D24';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B12':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B12':
       return '#BF8B2E';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B13':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B13':
       return '#3F7E44';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B14':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B14':
       return '#0A97D9';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B15':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B15':
       return '#56C02B';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B16':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B16':
       return '#00689D';
-    case 'http://www.semanticweb.org/aga/ontologies/2017/9/SDG#B17':
+    case 'http://www.semanticweb.org/TRDK2_graph-ROS/ontologies/2023/inf#B17':
       return '#19486A';
 
     default:
