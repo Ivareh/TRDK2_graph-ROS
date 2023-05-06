147942bd05e8eb0858ace1cd9dbff9158d5599df
Ivareh <69577035+Ivareh@users.noreply.github.com>
2023-05-06 13:39:09 +0200

    Added get all uncertainties query

---
+++ backend/src/database/queries/getAllUncertainties.ts
@@ -0,0 +1,14 @@
+import { parsePrefixesToQuery } from '../../common/database';
+import { PREFIXES } from '../index';
+
+export default (): string => {
+  const prefixString = parsePrefixesToQuery(PREFIXES.TRDK2, PREFIXES.RDF, PREFIXES.SCHEMA, PREFIXES.XSD, PREFIXES.RDFS);
+
+  return `
+      ${prefixString}
+      SELECT ?instancesOf ?label 
+      WHERE { 
+          ?instancesOf  rdf:type TRDK2:AllUncertainties.
+      } ORDER BY ( xsd:string ( STRBEFORE ( STR ( ?instancesOf ), "B" ) ) )
+      ( xsd:long ( STRAFTER ( STR ( ?instancesOf ), "B" ) ) )
+};
